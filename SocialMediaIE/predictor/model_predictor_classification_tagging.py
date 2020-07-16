acfa6575cab65b62dc1062f602c0ed75a3d84928
Shubhanshu Mishra <smishra8@illinois.edu>
2020-07-16 01:47:53 -0500

    Removed cuda dependencies

--- SocialMediaIE/predictor/model_predictor_classification_tagging.py
+++ SocialMediaIE/predictor/model_predictor_classification_tagging.py
@@ -165,10 +165,11 @@ def run(args):
     # encoder = PassThroughEncoder(ELMO_EMBEDDING_DIM)
     model = MultiTaskCRFTaggerAndClassifier(word_embeddings, encoders, vocab, TASKS)
     model.load_state_dict(torch.load(os.path.join(SERIALIZATION_DIR, "best.th")))
-    model = model.cuda(device=CUDA_DEVICE)
+    if args.cuda:
+        model = model.cuda(device=CUDA_DEVICE)
 
-    # Empty cache to ensure larger batch can be loaded for testing
-    torch.cuda.empty_cache()
+        # Empty cache to ensure larger batch can be loaded for testing
+        torch.cuda.empty_cache()
     logger.info("Evaluating on test data")
 
     test_iterator = CustomHomogeneousBatchIterator(
